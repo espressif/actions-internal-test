8648b984da72a706b927f17fcacb383785a406c5
Laukik Hase <laukik.hase@espressif.com>
2021-11-10 11:24:04 +0530

    Sync labeled PRs

--- github_pr_to_internal_pr/github_pr_to_internal_pr.py
+++ github_pr_to_internal_pr/github_pr_to_internal_pr.py
@@ -13,37 +13,26 @@ import requests
 from git import Git, Repo
 
 
-def pr_check_if_approved(pr_reviews_url):
-    print('Checking if PR has been approved...')
+def pr_check_approver(pr_creator, pr_comments_url, pr_approve_labeller):
+    print('Checking PR comment and affixed label...')
     # Requires Github Access Token, with Push Access
     GITHUB_TOKEN = os.environ['GITHUB_TOKEN']
 
-    r = requests.get(pr_reviews_url, headers={'Authorization': 'token ' + GITHUB_TOKEN})
-    r_data = r.json()
-    for review in r_data:
-        if review['state'] == 'APPROVED':
-            return review['user']['login']
-    
-    raise SystemError("Invalid operation! Please approve PR before trying to sync!")
-    
-    
-def pr_check_approver_access(project_users_url, pr_approver, pr_commenter):
-    print('Checking if PR approver access level matches criteria...')
-    # Requires Github Access Token, with Push Access
-    GITHUB_TOKEN = os.environ['GITHUB_TOKEN']
-
-    # NOTE: General form is: https://api.github.com/repos/org/repo/collaborators{/collaborator}, hence stripping the end
-    project_users_url = project_users_url.split('{/')[0]
-
-    r = requests.get(project_users_url, headers={'Authorization': 'token ' + GITHUB_TOKEN})
+    r = requests.get(pr_comments_url, headers={'Authorization': 'token ' + GITHUB_TOKEN})
     r_data = r.json()
 
-    pr_appr_perm = [usr for usr in r_data if usr['login'] == pr_approver][0]['permissions']
-    if not pr_appr_perm['triage']:
-        raise SystemError("PR Approver Access is below TRIAGE level!")
+    err = 0
+    for comment in reversed(r_data):
+        comment_body = comment['body']
+        if comment_body.startswith('sha='):
+            err= 1
+            if comment['user']['login'] == pr_approve_labeller != pr_creator:
+                return comment_body[4 : ]
 
-    if pr_commenter != pr_approver:
-        raise SystemError("PR Approver and the workflow initiator (with comment) must be the same!")
+    if err == 1:
+        raise SystemError("PR Comment Error: PR commenter and labeller do not match!")
+
+    raise SystemError("PR Comment Error: Command comment does not exist!")
 
 
 def pr_check_forbidden_files(pr_files_url):
@@ -60,15 +49,14 @@ def pr_check_forbidden_files(pr_files_url):
         raise SystemError("PR modifying forbidden files!!!")
 
 
-def pr_fetch_details(pr_url):
-    print('Fetching required PR details...')
-    # Requires Github Access Token, with Push Access
+def get_github_remote(project_html_url):
+    GITHUB_REMOTE_URL = project_html_url
+
+    HDR_LEN = 8
     GITHUB_TOKEN = os.environ['GITHUB_TOKEN']
+    gh_remote = GITHUB_REMOTE_URL[: HDR_LEN] + GITHUB_TOKEN + ':' + GITHUB_TOKEN + '@' + GITHUB_REMOTE_URL[HDR_LEN :]
 
-    r = requests.get(pr_url, headers={'Authorization': 'token ' + GITHUB_TOKEN})
-    r_data = r.json()
-
-    return r_data
+    return gh_remote
 
 
 def setup_project(project_fullname):
@@ -101,34 +89,78 @@ def check_remote_branch(project, pr_branch):
     raise SystemError("PR branch creation failed!")
 
 
-# Merge PRs with/without Rebase
-def sync_pr(project_name, pr_num, pr_branch, pr_commit_id, project_html_url, pr_html_url, rebase_flag):
-    GITHUB_REMOTE_NAME = 'github'
-    GITHUB_REMOTE_URL = project_html_url
+def check_update_label(pr_label, pr_labels_list):
+    LABEL_MERGE = 'PR-Sync-Merge'
+    LABEL_REBASE = 'PR-Sync-Rebase'
 
-    HDR_LEN = 8
-    GITHUB_TOKEN = os.environ['GITHUB_TOKEN']
-    gh_remote = GITHUB_REMOTE_URL[: HDR_LEN] + GITHUB_TOKEN + ':' + GITHUB_TOKEN + '@' + GITHUB_REMOTE_URL[HDR_LEN :]
+    label_validity = [label['name'] for label in pr_labels_list
+            if label['name'] == LABEL_MERGE or label['name'] == LABEL_REBASE]
+
+    if not label_validity:
+        raise SystemError('PR-Sync-Update Label: Illegal use!')
+
+
+# Update existing MR
+def update_mr(project_name, pr_num, pr_branch, pr_commit_id, project_html_url, project_gl):
+    branch = project_gl.branches.get(pr_branch)
+    if not branch:
+        raise SystemError("PR Update: No branch found on internal remote to update!")
+
+    GITHUB_REMOTE_NAME = 'github'
+    gh_remote = get_github_remote(project_html_url)
+    git = Git(project_name)
+
+    print('Adding the Github remote...')
+    git.remote('add', GITHUB_REMOTE_NAME, gh_remote)
+    git.checkout(pr_branch)
+
+    print('Checking out to master...')
+    git.checkout('master')
+
+    print('Updating the PR branch...')
+    git.branch('--delete', pr_branch)
+    git.fetch(GITHUB_REMOTE_NAME, 'pull/' + str(pr_num) + '/head')
+    git.checkout('FETCH_HEAD', b=pr_branch)
+
+    print('Checking whether specified commit ID matches with user branch HEAD...')
+    expected_commit_id = git.rev_parse('--short', 'HEAD')
+    print('exp: ', expected_commit_id, len(expected_commit_id))
+    print('cur: ', pr_commit_id, len(pr_commit_id))
+    if pr_commit_id.startswith(expected_commit_id):
+        raise SystemError("PR Commit SHA1 in workflow comment and user branch do not match!")
+
+    print('Pushing to remote...')
+    git.push('--force', 'origin', pr_branch)
+
+
+# Merge PRs with/without Rebase
+def sync_pr(project_name, pr_num, pr_branch, pr_commit_id, project_html_url, project_gl, pr_html_url, rebase_flag):
+    branch = project_gl.branches.get(pr_branch)
+    if branch:
+        raise SystemError("PR Merge/Rebase: Branch/MR already exists for PR!")
+
+    GITHUB_REMOTE_NAME = 'github'
+    gh_remote = get_github_remote(project_html_url)
 
     git = Git(project_name)
 
     print('Checking out to master branch...')
-    print(git.checkout('test_master'))
+    git.checkout('test_master')
 
     print('Adding the Github remote...')
-    print(git.remote('add', GITHUB_REMOTE_NAME, gh_remote))
+    git.remote('add', GITHUB_REMOTE_NAME, gh_remote)
 
     print('Fetching the PR branch...')
-    print(git.fetch(GITHUB_REMOTE_NAME, 'pull/' + str(pr_num) + '/head'))
+    git.fetch(GITHUB_REMOTE_NAME, 'pull/' + str(pr_num) + '/head')
 
     print('Checking out the PR branch...')
-    print(git.checkout('FETCH_HEAD', b=pr_branch))
-   
+    git.checkout('FETCH_HEAD', b=pr_branch)
+
     print('Checking whether specified commit ID matches with user branch HEAD...')
     expected_commit_id = git.rev_parse('--short', 'HEAD')
 
-    if pr_commit_id != expected_commit_id:
-        raise SystemError("PR Commit SHA ID in workflow comment and user branch do not match!")
+    if pr_commit_id.startswith(expected_commit_id):
+        raise SystemError("PR Commit SHA1 in workflow comment and user branch do not match!")
 
     if rebase_flag:
         #  Set the config parameters: Better be a espressif bot
@@ -137,16 +169,16 @@ def sync_pr(project_name, pr_num, pr_branch, pr_commit_id, project_html_url, pr_
         repo.config_writer().set_value('user', 'email', os.environ['GIT_CONFIG_EMAIL']).release()
 
         print('Rebasing with the latest master...')
-        print(git.rebase('test_master'))
+        git.rebase('test_master')
 
         commit = repo.head.commit
         new_cmt_msg = commit.message + '\nMerges ' + pr_html_url
 
         print('Amending commit message (Adding additional info about commit)...')
-        print(git.execute(['git','commit', '--amend', '-m', new_cmt_msg]))
+        git.execute(['git','commit', '--amend', '-m', new_cmt_msg])
 
     print('Pushing to remote...')
-    print(git.push('--set-upstream', 'origin', pr_branch))
+    git.push('--set-upstream', 'origin', pr_branch)
 
 
 def main():
@@ -163,46 +195,41 @@ def main():
         event = json.load(f)
         # print(json.dumps(event, indent=4))
 
+    LABEL_MERGE = 'PR-Sync-Merge'
+    LABEL_REBASE = 'PR-Sync-Rebase'
+    LABEL_UPDATE = 'PR-Sync-Update'
+
+    pr_label = event['label']['name']
+    pr_labels_list = event['pull_request']['labels']
+    if pr_label == LABEL_UPDATE:
+        check_update_label(pr_label, pr_labels_list)
+
+    pr_approve_labeller = event['sender']['login']
+    pr_creator = event['pull_request']['user']['login']
+    pr_comments_url = event['pull_request']['comments_url']
+    # Checks whether the approve labeller and workflow initiator are the same
+    pr_commit_id = pr_check_approver(pr_creator, pr_comments_url, pr_approve_labeller)
+
     project_fullname = event['repository']['full_name']
     project_org, project_name = project_fullname.split('/')
     project_users_url = event['repository']['collaborators_url']
     project_html_url = event['repository']['clone_url']
 
-    pr_num = event['issue']['number']
+    pr_num = event['pull_request']['number']
     pr_branch = 'contrib/github_pr_' + str(pr_num)
-    pr_rest_url = event['issue']['pull_request']['url'] 
-    pr_html_url = event['issue']['pull_request']['html_url']
-
-    pr_review_body = event['comment']['body']
-    idx = pr_review_body.find('sha')
-    pr_review_cmd = pr_review_body[0 : idx - 1] 
-    pr_commit_id = pr_review_body[idx + 4 : ]
-    pr_commenter = event['comment']['user']['login']
-
-    pr_reviews_url = pr_rest_url + '/reviews'
-    # Check whether the PR has been approved or not
-    pr_approver = pr_check_if_approved(pr_reviews_url)
-
-    # Checks whether the approver access level is above required; needs Github access token
-    pr_check_approver_access(project_users_url, pr_approver, pr_commenter)
+    pr_rest_url = event['pull_request']['url']
+    pr_html_url = event['pull_request']['html_url']
 
     pr_files_url = pr_rest_url + '/files'
     # Check whether the PR has modified forbidden files
     pr_check_forbidden_files(pr_files_url)
 
-    #  Fetching required PR details
-    pr_data = pr_fetch_details(pr_rest_url)
-
-    pr_target_branch = pr_data['base']['ref']
-    # if pr_target_branch != 'master':
-    #     raise SystemError("Illegal base branch - set it to master!")
-
     # Getting the PR title and body
-    pr_title = pr_data['title']
-    idx = pr_title.find(os.environ['JIRA_PROJECT']) # Finding the JIRA issue tag
-    pr_title_desc = pr_title[0 : idx - 2] # For space character
+    pr_title = event['pull_request']['title']
+    idx = pr_title.find(os.environ['JIRA_PROJECT'])  # Finding the JIRA issue tag
+    pr_title_desc = pr_title[0 : idx - 2]  # For space character
     pr_jira_issue = pr_title[idx : -1]
-    pr_body = pr_data['body']
+    pr_body = event['pull_request']['body']
 
     # NOTE: Modified for testing purpose
     project_fullname = 'app-frameworks/actions-internal-test'
@@ -211,23 +238,24 @@ def main():
     gl = setup_project(project_fullname)
     project_gl = gl.projects.get(project_fullname)
 
-    if pr_review_cmd.startswith('/rebase'):
+    if pr_label == LABEL_REBASE:
         sync_pr(project_name, pr_num, pr_branch, pr_commit_id, project_html_url, pr_html_url, rebase_flag=True)
-    elif pr_review_cmd.startswith('/merge'):
+    elif pr_label == LABEL_MERGE:
         sync_pr(project_name, pr_num, pr_branch, pr_commit_id, project_html_url, pr_html_url, rebase_flag=False)
-    else:
-        print('No action selected!!!')
+    elif pr_label == LABEL_UPDATE:
+        update_mr(project_name, pr_num, pr_branch, pr_commit_id, project_html_url, project_gl)
+        print('Done with the workflow!')
         return
+    else:
+        raise SystemError("Illegal program flow!")
 
     # Deleting local repo
     shutil.rmtree(project_name)
 
-    print('Creating a merge request...')
-    project_gl = gl.projects.get(project_fullname)
-
     # NOTE: Remote takes some time to register a branch
     time.sleep(15)
 
+    print('Creating a merge request...')
     mr = project_gl.mergerequests.create({'source_branch': pr_branch, 'target_branch': 'test_master', 'title': pr_title_desc})
 
     print('Updating merge request description...')
